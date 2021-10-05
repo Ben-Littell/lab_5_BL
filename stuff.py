4db0db3c6423b3916bcd2d92bf5a5bbe11773b0c
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2021-10-05 09:38:30 -0400

    cone and body partial thruster

--- stuff.py
+++ stuff.py
@@ -12,12 +12,43 @@ COLORS = [RED, GREEN, BLUE, BLACK]
 
 # Math Constants
 PI = math.pi
-
+HEIGHT = 500
+WIDTH = 700
 # Game Constants
-SIZE = (700, 500)
+SIZE = (WIDTH, HEIGHT)
 FPS = 60
 
 ########################################################################################################################
+
+
+class Rocket:
+    def __init__(self, height, width, center, color1, color2):
+        self.height = height
+        self.width = width
+        self.color1 = color1
+        self.color2 = color2
+        self.center = center
+
+    def draw_rocket(self):
+        # cone
+        pygame.draw.polygon(screen, BLACK, [(self.center-self.width//2, self.height+50),
+                                            (self.center+self.width//2, self.height+50),
+                                            (self.center, self.height)])
+        # body
+        pygame.draw.rect(screen, BLACK, (self.center-self.width//2, self.height+50, self.width, self.height-25))
+        # thruster
+        pygame.draw.polygon(screen, BLACK, [(self.center-self.width//2, self.height+150),
+                                            (self.center+self.width//2, self.height+150),
+                                            (self.center-self.width//2-15, self.height+150)])
+
+
+
+
+
+
+
+
+
 ########################################################################################################################
 
 pygame.init()
@@ -26,7 +57,7 @@ screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Animation Intro')
 
 clock = pygame.time.Clock()
-
+rocket1 = Rocket(200, 100, 350, BLACK, RED)
 running = True
 # game loop
 while running:
@@ -36,7 +67,7 @@ while running:
             running = False
 
     screen.fill(WHITE)
-
+    rocket1.draw_rocket()
     pygame.display.flip()
 
     clock.tick(FPS)
